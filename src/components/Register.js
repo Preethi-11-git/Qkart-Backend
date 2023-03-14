392ae8c5693781333b03f0f72851fa42f45de6b8
CrioUser <crio-user@criodo.com>
2023-03-14 22:43:53 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,3 +1,4 @@
+import { ConstructionOutlined } from "@mui/icons-material";
 import { Button, CircularProgress, Stack, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
@@ -9,14 +10,17 @@ import Header from "./Header";
 import "./Register.css";
 
 const Register = () => {
+  const[formData,setFormData]=useState({
+    username: "",
+    password: "",
+  });
   const { enqueueSnackbar } = useSnackbar();
-
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
   /**
    * Definition for register handler
    * - Function to be called when the user clicks on the register button or submits the register form
-   *
+   * 
    * @param {{ username: string, password: string, confirmPassword: string }} formData
    *  Object with values of username, password and confirm password user entered to register
    *
@@ -35,7 +39,39 @@ const Register = () => {
    *      "message": "Username is already taken"
    * }
    */
+   const handleInputChange=(event)=>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
+    console.log(formData);
+   }
   const register = async (formData) => {
+   
+      // POST request using axios with async/await
+      // console.log(formData);
+      // console.log(formData.formData.username);
+      // const body = {"username":formData.username,"password":formData.password}
+      // console.log("here");
+      // console.log(typeof body);
+      // console.log(body);
+      let url=`${config.endpoint}/auth/register`
+      try
+      {
+        
+      await axios.post(url,{
+        username: formData.username,
+        password: formData.password
+      });
+      
+      enqueueSnackbar("Registered successfully",{variant:"success"});
+      }
+      catch(err)
+      {
+        console.log("error")
+        console.log(err);
+      }
+
+
   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
@@ -68,7 +104,7 @@ const Register = () => {
     >
       <Header hasHiddenAuthButtons />
       <Box className="content">
-        <Stack spacing={2} className="form">
+        <Stack spacing={2} className="form" >
           <h2 className="title">Register</h2>
           <TextField
             id="username"
@@ -76,7 +112,9 @@ const Register = () => {
             variant="outlined"
             title="Username"
             name="username"
+            value={formData.username}
             placeholder="Enter Username"
+            onChange={(e) => handleInputChange(e)}
             fullWidth
           />
           <TextField
@@ -85,6 +123,8 @@ const Register = () => {
             label="Password"
             name="password"
             type="password"
+            value={formData.password}
+            onChange={(e) => handleInputChange(e)}
             helperText="Password must be atleast 6 characters length"
             fullWidth
             placeholder="Enter a password with minimum 6 characters"
@@ -97,7 +137,7 @@ const Register = () => {
             type="password"
             fullWidth
           />
-           <Button className="button" variant="contained">
+           <Button className="button" variant="contained" onClick={()=>register(formData)}>
             Register Now
            </Button>
           <p className="secondary-action">
